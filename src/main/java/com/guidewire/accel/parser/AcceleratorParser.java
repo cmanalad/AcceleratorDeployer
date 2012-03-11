645c59c5db4a2481e5324fbfc9df9b8f07c8f31a
Allen Fogleson <afogleson@guidewire.com>
2012-03-11 18:19:02 +0000

    gosu and gunt parsed - need unit tests

--- src/main/java/com/guidewire/accel/parser/AcceleratorParser.java
+++ src/main/java/com/guidewire/accel/parser/AcceleratorParser.java
@@ -2,6 +2,7 @@ package com.guidewire.accel.parser;
 
 import com.guidewire.accel.deployment.DeployableComponent;
 import com.guidewire.accel.deployment.impl.AntBuildComponent;
+import com.guidewire.accel.deployment.impl.GosuComponent;
 import com.guidewire.accel.deployment.impl.MavenBuildComponent;
 import com.guidewire.accel.deployment.impl.PCFComponent;
 import com.guidewire.accel.util.FileUtil;
@@ -35,7 +36,7 @@ public class AcceleratorParser {
   }
 
   public File getDeploymentDescriptor() {
-    return new File( accelRoot.getAbsolutePath() + File.separator + "deployment.xml");
+    return new File(accelRoot.getAbsolutePath() + File.separator + "deployment.xml");
   }
 
   public void parseAccelerator() {
@@ -53,11 +54,11 @@ public class AcceleratorParser {
       // Print the document from the DOM tree and
       //   feed it an initial indentation of nothing
       NodeList elements = doc.getChildNodes();
-      for(int i=0; i<elements.getLength(); i++) {
+      for (int i = 0; i < elements.getLength(); i++) {
         Node node = elements.item(i);
-        if(node.getNodeName().trim().equals("accelerator")) {
+        if (node.getNodeName().trim().equals("accelerator")) {
           NodeList list = node.getChildNodes();
-          for(int j=0; j<list.getLength(); j++) {
+          for (int j = 0; j < list.getLength(); j++) {
             parse(list.item(j));
           }
         }
@@ -77,52 +78,80 @@ public class AcceleratorParser {
 
   private void parse(Node node) {
     //For every node except accelerator we should be creating a deployable component.
-    if(node.getNodeName().equals("mavenBuild")) {
+    if (node.getNodeName().equals("mavenBuild")) {
       String directory = accelRoot.getAbsolutePath() + File.separator;
       String goals = "";
       NodeList children = node.getChildNodes();
-      for(int i=0; i<children.getLength(); i++) {
+      for (int i = 0; i < children.getLength(); i++) {
         Node n = children.item(i);
-        if(n.getNodeName().trim().equals("directory")) {
+        if (n.getNodeName().trim().equals("directory")) {
           directory = directory + n.getFirstChild().getNodeValue().trim() + File.separator;
-        }
-        else if(n.getNodeName().trim().equals("goals")) {
+        } else if (n.getNodeName().trim().equals("goals")) {
           goals = n.getFirstChild().getNodeValue();
         }
       }
       MavenBuildComponent component = new MavenBuildComponent(new File(directory), goals);
       addToComponents(component);
-    }
-    else if(node.getNodeName().equals("antBuild")) {
+    } else if (node.getNodeName().equals("antBuild")) {
       String directory = accelRoot.getAbsolutePath() + File.separator;
       File buildFile = new File(directory + "build.xml");
       String target = "";
       NodeList children = node.getChildNodes();
-      for(int i=0; i<children.getLength(); i++) {
+      for (int i = 0; i < children.getLength(); i++) {
         Node n = children.item(i);
-        if(n.getNodeName().trim().equals("target")) {
+        if (n.getNodeName().trim().equals("target")) {
           target = n.getFirstChild().getNodeValue().trim();
-        }
-        else if(n.getNodeName().trim().equals("buildFile")) {
+        } else if (n.getNodeName().trim().equals("buildFile")) {
           directory = directory + n.getFirstChild().getNodeValue().trim();
           buildFile = new File(directory);
         }
       }
       AntBuildComponent component = new AntBuildComponent(buildFile, target);
       addToComponents(component);
-    }
-    else if(node.getNodeName().equals("pcfRoot")) {
+    } else if (node.getNodeName().equals("pcfRoot")) {
       String directory = accelRoot.getAbsolutePath() + File.separator;
       NodeList childDirs = node.getChildNodes();
-      for(int i=0; i<childDirs.getLength(); i++) {
+      for (int i = 0; i < childDirs.getLength(); i++) {
         Node pcfDir = childDirs.item(i);
-        if(node.getNodeName().equals("directory")) {
+        if (node.getNodeName().equals("directory")) {
           String pcfDirPath = node.getFirstChild().getNodeValue().trim();
           File pcfDirectory = new File(directory + pcfDirPath);
           //Get every file in the config directory....
           File[] pcfFiles = FileUtil.listFiles(pcfDirectory);
-          for(File f : pcfFiles) {
-            PCFComponent component = new PCFComponent(f, accelRoot);
+          for (File f : pcfFiles) {
+            PCFComponent component = new PCFComponent(f, new File(pcfDirPath));
+            addToComponents(component);
+          }
+        }
+      }
+    } else if (node.getNodeName().trim().equals("gosuRoot")) {
+      String directory = accelRoot.getAbsolutePath() + File.separator;
+      NodeList childDirs = node.getChildNodes();
+      for (int i = 0; i < childDirs.getLength(); i++) {
+        Node gosuDir = childDirs.item(i);
+        if (node.getNodeName().equals("directory")) {
+          String gosuDirPath = node.getFirstChild().getNodeValue().trim();
+          File gosuDirectory = new File(directory + gosuDirPath);
+          //Get every file in the config directory....
+          File[] pcfFiles = FileUtil.listFiles(gosuDirectory);
+          for (File f : pcfFiles) {
+            GosuComponent component = new GosuComponent(f, false);
+            addToComponents(component);
+          }
+        }
+      }
+    } else if (node.getNodeName().trim().equals("gunitRoot")) {
+      String directory = accelRoot.getAbsolutePath() + File.separator;
+      NodeList childDirs = node.getChildNodes();
+      for (int i = 0; i < childDirs.getLength(); i++) {
+        Node gosuDir = childDirs.item(i);
+        if (node.getNodeName().equals("directory")) {
+          String gosuDirPath = node.getFirstChild().getNodeValue().trim();
+          File gosuDirectory = new File(directory + gosuDirPath);
+          //Get every file in the config directory....
+          File[] pcfFiles = FileUtil.listFiles(gosuDirectory);
+          for (File f : pcfFiles) {
+            GosuComponent component = new GosuComponent(f, true);
             addToComponents(component);
           }
         }
@@ -131,7 +160,7 @@ public class AcceleratorParser {
   }
 
   private void addToComponents(DeployableComponent component) {
-    if(components == null) {
+    if (components == null) {
       components = new ArrayList<DeployableComponent>();
     }
     components.add(component);
