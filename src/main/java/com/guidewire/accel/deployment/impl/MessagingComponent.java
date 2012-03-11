853f07f65c01dfb77a5f9a6b980971b3fc5b030e
Allen Fogleson <afogleson@guidewire.com>
2012-03-11 22:46:15 +0000

    some more work on messaging deployer

--- src/main/java/com/guidewire/accel/deployment/impl/MessagingComponent.java
+++ src/main/java/com/guidewire/accel/deployment/impl/MessagingComponent.java
@@ -1,6 +1,8 @@
 package com.guidewire.accel.deployment.impl;
 
 import com.guidewire.accel.deployment.DeployableComponent;
+import com.guidewire.accel.util.NameValuePair;
+import com.sun.corba.se.impl.orbutil.fsm.NameBase;
 
 import java.util.ArrayList;
 
@@ -11,15 +13,23 @@ import java.util.ArrayList;
  */
 public class MessagingComponent implements DeployableComponent {
 
-  private String reqquestName;
+  private String registryDir = "modules/configuration/config/plguin/registry";
+
+  private String requestName;
   private MessageImplementationType requestType;
   private String requestClass;
+  private String requestPluginDir;
+  private ArrayList<NameValuePair> requestParams;
   private String transportName;
   private MessageImplementationType transportType;
   private String transportClass;
-  private String replytName;
+  private String transportPluginDir;
+  private ArrayList<NameValuePair> transportParams;
+  private String replyName;
   private MessageImplementationType replyType;
   private String replyClass;
+  private String replyPluginDir;
+  private ArrayList<NameValuePair> replyParams;
   private int pollInterval = 10000;
   private int initialRetryInterval = 1000;
   private int maxRetries = 3;
@@ -137,8 +147,123 @@ public class MessagingComponent implements DeployableComponent {
     }
   }
 
+  public void addRequestParameter(NameValuePair nvp) {
+    if(requestParams == null) {
+      requestParams = new ArrayList<NameValuePair>();
+    }
+    requestParams.add(nvp);
+  }
+
+  public void addTransportParameter(NameValuePair nvp) {
+    if(transportParams == null) {
+      transportParams = new ArrayList<NameValuePair>();
+    }
+    transportParams.add(nvp);
+  }
+  public void addReplyParameter(NameValuePair nvp) {
+    if(replyParams == null) {
+      replyParams = new ArrayList<NameValuePair>();
+    }
+    replyParams.add(nvp);
+  }
+
+  /**
+   *
+   * We need to do something like this....
+   * <?xml version="1.0"?>
+   * <plugin
+   *   interface="MessageTransport"
+   *   name="TestTransport">
+   *   <plugin-java
+   *     javaclass="com.lv.nucleus.java.transport.TestTransport"
+   *     plugindir="pidir"/>
+   * </plugin>
+   * or
+   *
+   * <plugin
+   *   interface="MessageTransport"
+   *   name="CueTransportPlugin">
+   *   <plugin-gosu
+   *     gosuclass="com.lv.nucleus.cue.messaging.plugins.CUETransportPlugin">
+   *     <param
+   *       name="UserName"
+   *       value="su"/>
+   *     <param
+   *       name="Password"
+   *       value="gw"/>
+   *   </plugin-gosu>
+   * </plugin>
+   *
+   * For each of the plugins. we also need to set up the message queue.
+   *
+   * @return true if we succeed doing all this.
+   */
+
+  private String generatePluginXml(String name,                     String className,
+                                   ArrayList<NameValuePair> params, MessageImplementationType type,
+                                   String pluginDir,                String pluginType) {
+    StringBuilder xml = new StringBuilder();
+    xml.append("<?xml version=\"1.0\"?>\n");
+    xml.append("<plugin\n  interface=\"Message");
+    xml.append(pluginType);
+    xml.append("\"\n  name=\"");
+    xml.append(name);
+    xml.append("\">\n");
+    if(type == MessageImplementationType.gosu) {
+      xml.append("  <plugin-gosu\n    gosuclass=\"");
+      xml.append(className);
+      xml.append("\">\n");
+    }
+    else {
+      xml.append("  <plugin-java\n    javaclass=\"");
+      xml.append(className);
+      xml.append("\"");
+      if(pluginDir != null) {
+        xml.append("\n    plugindir=\"");
+        xml.append(pluginDir);
+        xml.append("\"");
+      }
+      xml.append(">\n");
+    }
+    //Handle parameters
+    if(params != null && !params.isEmpty()) {
+      for(NameValuePair nvp : params) {
+        xml.append("    <param\n      name=\"");
+        xml.append(nvp.getName());
+        xml.append("\"\n      value=\"");
+        xml.append(nvp.getValue());
+        xml.append("\"/>\n");
+      }
+    }
+    if(type == MessageImplementationType.gosu) {
+      xml.append("</plugin-gosu>\n</plugin>");
+    }
+    else {
+      xml.append("</plugin-java>\n</plugin>");
+    }
+    return xml.toString();
+  }
+
+
   @Override
   public boolean deploy() {
+    String requestPluginXml   = null;
+    String transportPluginXml = null;
+    String replyPluginXml     = null;
+
+    if(requestClass != null) {
+      requestPluginXml = generatePluginXml(requestName, requestClass, requestParams, requestType, requestPluginDir, "Request");
+    }
+    if(transportClass != null) {
+      transportPluginXml = generatePluginXml(transportName, transportClass, transportParams, transportType, transportPluginDir, "Transport");
+    }
+    if(replyClass != null) {
+      replyPluginXml = generatePluginXml(replyName, replyClass, replyParams, replyType, replyPluginDir, "Reply");
+    }
+
+    //plugins go in productRoot + registrydir + pluginName + .xml
+
+
     return false;
   }
 }
