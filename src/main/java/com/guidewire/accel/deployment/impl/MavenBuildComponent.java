8f6fb205ba37c4c4dfcc0eae128dbe2485c0f6a2
Allen Fogleson <afogleson@guidewire.com>
2012-03-11 09:46:38 +0000

    Maven build component and test

--- src/main/java/com/guidewire/accel/deployment/impl/MavenBuildComponent.java
+++ src/main/java/com/guidewire/accel/deployment/impl/MavenBuildComponent.java
@@ -1,15 +1,17 @@
 package com.guidewire.accel.deployment.impl;
 
 import com.guidewire.accel.deployment.DeployableComponent;
+import org.apache.maven.shared.invoker.*;
 
 import java.io.File;
+import java.util.Collections;
 
 /**
- *
  * User: afogleson
  * Date: 3/11/12
  * Time: 12:52 AM
- *
+ * <p/>
+ * invoke a maven build process.
  */
 public class MavenBuildComponent implements DeployableComponent {
 
@@ -27,6 +29,21 @@ public class MavenBuildComponent implements DeployableComponent {
 
   @Override
   public boolean deploy() {
-    return false;
+    boolean deployed = true;
+    try {
+      InvocationRequest request = new DefaultInvocationRequest();
+      request.setPomFile(pom);
+      request.setGoals(Collections.singletonList(goals));
+
+      Invoker invoker = new DefaultInvoker();
+      InvocationResult result = invoker.execute(request);
+      if (result.getExitCode() != 0) {
+        deployed = false;
+      }
+    }
+    catch (Throwable t) {
+      deployed = false;
+    }
+    return deployed;
   }
 }
---
+++ src/test/java/com/guidewire/com/guidewire/accel/deployment/impl/MavenBuildComponentTest.java
@@ -0,0 +1,29 @@
+package com.guidewire.com.guidewire.accel.deployment.impl;
+
+import com.guidewire.accel.deployment.impl.MavenBuildComponent;
+import junit.framework.TestCase;
+
+import java.io.File;
+
+/**
+ *
+ * User: afogleson
+ * Date: 3/11/12
+ * Time: 1:21 AM
+ *
+ */
+public class MavenBuildComponentTest extends TestCase {
+
+  public void testMavenBuildFailure() {
+    File pom = new File("src/test/resources/pom.xml");
+    MavenBuildComponent component = new MavenBuildComponent(pom);
+    assertFalse(component.deploy());
+  }
+
+  public void testMavenBuildSuccess() {
+    File pom = new File("src/test/resources/sample/pom.xml");
+    String goals = "clean";
+    MavenBuildComponent component = new MavenBuildComponent(pom, goals);
+    assertTrue(component.deploy());
+  }
+}
