3768ff1854423841018ff131ac3e3b6f2cd8ba8f
Allen Fogleson <afogleson@guidewire.com>
2012-03-11 21:01:04 +0000

    beginning of library component.

--- src/main/java/com/guidewire/accel/deployment/impl/LibraryComponent.java
+++ src/main/java/com/guidewire/accel/deployment/impl/LibraryComponent.java
@@ -2,6 +2,8 @@ package com.guidewire.accel.deployment.impl;
 
 import com.guidewire.accel.deployment.DeployableComponent;
 
+import java.io.File;
+
 /**
  * User: afogleson
  * Date: 3/10/12
@@ -9,9 +11,23 @@ import com.guidewire.accel.deployment.DeployableComponent;
  */
 public class LibraryComponent implements DeployableComponent {
 
+  private File libraryFile;
+  //If pluginDir is null then we will copy to configuration/plugins/shared/lib
+  //otherwise you copy them into the plugin dir under configuration/plugins/pluginDir
+  private String pluginDir;
+
+  public LibraryComponent(File libraryFile) {
+    this.libraryFile = libraryFile;
+  }
+
+  public LibraryComponent(File libraryFile, String pluginDir) {
+    this.libraryFile = libraryFile;
+    this.pluginDir = pluginDir;
+  }
+
   @Override
   public boolean deploy() {
-    return false;  //To change body of implemented methods use File | Settings | File Templates.
+    return false;
   }
 
 
