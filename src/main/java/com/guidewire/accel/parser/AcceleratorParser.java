b201dc0c993ddabfa9c4c500ca4aaa2bfba16736
Allen Fogleson <afogleson@guidewire.com>
2012-03-11 11:39:12 +0000

    first but of the parser. only parses maven and ant build components. enough to pass test right now, proving the parsing works. Still not validating

--- src/main/java/com/guidewire/accel/parser/AcceleratorParser.java
+++ src/main/java/com/guidewire/accel/parser/AcceleratorParser.java
@@ -1,7 +1,16 @@
 package com.guidewire.accel.parser;
 
 import com.guidewire.accel.deployment.DeployableComponent;
+import com.guidewire.accel.deployment.impl.AntBuildComponent;
+import com.guidewire.accel.deployment.impl.MavenBuildComponent;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.util.ArrayList;
 
@@ -23,9 +32,90 @@ public class AcceleratorParser {
     return components;
   }
 
+  public File getDeploymentDescriptor() {
+    return new File( accelRoot.getAbsolutePath() + File.separator + "deployment.xml");
+  }
+
   public void parseAccelerator() {
     //parse through the deployment descriptor, add all the components to the list
+    try {
+      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+      // Turn on validation, and turn off namespaces
+      factory.setValidating(false);
+      factory.setNamespaceAware(false);
+
+      DocumentBuilder builder = factory.newDocumentBuilder();
+
+      Document doc = builder.parse(getDeploymentDescriptor());
+
+      // Print the document from the DOM tree and
+      //   feed it an initial indentation of nothing
+      NodeList elements = doc.getChildNodes();
+      for(int i=0; i<elements.getLength(); i++) {
+        Node node = elements.item(i);
+        if(node.getNodeName().trim().equals("accelerator")) {
+          NodeList list = node.getChildNodes();
+          for(int j=0; j<list.getLength(); j++) {
+            parse(list.item(j));
+          }
+        }
+
+      }
+    }
+    catch (ParserConfigurationException e) {
+      System.out.println("The underlying parser does not support the requested features.");
+    }
+    catch (FactoryConfigurationError e) {
+      System.out.println("Error occurred obtaining Document Builder Factory.");
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void parse(Node node) {
+    //For every node except accelerator we should be creating a deployable component.
+    if(node.getNodeName().equals("mavenBuild")) {
+      String directory = accelRoot.getAbsolutePath() + File.separator;
+      String goals = "";
+      NodeList children = node.getChildNodes();
+      for(int i=0; i<children.getLength(); i++) {
+        Node n = children.item(i);
+        if(n.getNodeName().trim().equals("directory")) {
+          directory = directory + n.getFirstChild().getNodeValue().trim() + File.separator;
+        }
+        else if(n.getNodeName().trim().equals("goals")) {
+          goals = n.getFirstChild().getNodeValue();
+        }
+      }
+      MavenBuildComponent component = new MavenBuildComponent(new File(directory), goals);
+      addToComponents(component);
+    }
+    else if(node.getNodeName().equals("antBuild")) {
+      String directory = accelRoot.getAbsolutePath() + File.separator;
+      File buildFile = new File(directory + "build.xml");
+      String target = "";
+      NodeList children = node.getChildNodes();
+      for(int i=0; i<children.getLength(); i++) {
+        Node n = children.item(i);
+        if(n.getNodeName().trim().equals("target")) {
+          target = n.getFirstChild().getNodeValue().trim();
+        }
+        else if(n.getNodeName().trim().equals("buildFile")) {
+          directory = directory + n.getFirstChild().getNodeValue().trim();
+          buildFile = new File(directory);
+        }
+      }
+      AntBuildComponent component = new AntBuildComponent(buildFile, target);
+      addToComponents(component);
+    }
 
   }
 
+  private void addToComponents(DeployableComponent component) {
+    if(components == null) {
+      components = new ArrayList<DeployableComponent>();
+    }
+    components.add(component);
+  }
 }
